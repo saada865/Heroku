413c77cb21d56299f6f763c0ddc45d552fe29621
saada865 <saadm865@gmail.com>
2024-08-21 12:27:37 +0500

    commit2

--- app.py
+++ app.py
@@ -6,6 +6,5 @@ app = Flask(__name__)
 def hello():
     return 'Hello World'
 
-
-if __name__ == 'main':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
